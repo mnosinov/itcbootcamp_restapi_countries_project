ebd4143439e223ea8610ad54b1cc9efaabb893ff
mnosinov <mnosinov@gmail.com>
2023-03-01 14:09:47 +0600

    themes switcher is ready to use

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// elements of view
+const themeSwitcherBtn = document.getElementById('themeSwitcherBtn');
+const themeIconImg = document.getElementById('themeIconImg');
+const themeTitleDiv = document.getElementById('themeTitleDiv');
+
 /* style themes ----------------------------BEGIN */
 let themes = [
 	{ name: 'theme-light', cssFile: 'css/style-theme-light.css', themeIcon: 'images/icon-theme-light.svg', title: 'Light Mode' },
@@ -18,36 +23,53 @@ function initThemes(defaultTheme) { // should be called once on page load.
 }
 
 function setNextTheme(defaultTheme=undefined) {
-	let currentTheme;
+	let currentTheme, nextTheme;
 	for (let cls of document.body.classList) {
-		if (themes.includes(cls)) {
-			currentTheme = cls;
-			break;
-		}
+		currentTheme = themes.find( element => element.name === cls );
+		if (currentTheme) break;
 	}
 	if (currentTheme === undefined) {
 		// if no default theme had been passed in, then default is 1st in themese list
 		// and check if defaultTheme is in themes
-		if (defaultTheme && themes.find( theme => theme === defaultTheme )) {
-			currentTheme = defaultTheme;
-		} else {
-			currentTheme = themes[0];
-		}
+		if (defaultTheme && themes.find( theme => theme === defaultTheme ))
+			nextTheme = defaultTheme;
+		else
+			nextTheme = themes[0];
 		// set initial theme
-		let indexOfCurrentTheme = themes.indexOf(currentTheme);
-		document.body.classList.add(themes[indexOfCurrentTheme].name);
 	} else {
 		// get the next theme in list cyclically
 		let indexOfCurrentTheme = themes.indexOf(currentTheme);
 		let nextThemeIndex = (indexOfCurrentTheme + 1) % themes.length;
 		// turn off previous theme, turn on the next theme
-		document.body.classList.remove(themes[indexOfCurrentTheme].name);
-		document.body.classList.add(themes[nextThemeIndex].name);
+		nextTheme = themes[nextThemeIndex];
 	}
+	// remove all themes from body's class list
+	themes.forEach( theme => { document.body.classList.remove(theme.name); });
+	// add the next theme class to body's class list
+	document.body.classList.add(nextTheme.name);
+	// set current theme switcher label
+	setCurrentThemeSwitcherLabel(nextTheme);
 }
 
+function setCurrentThemeSwitcherLabel(theme) {
+	if (theme) {
+		// get the next theme icon and label
+		let nextThemeIndex = (themes.indexOf(theme) + 1) % themes.length;
+		let nextTheme = themes[nextThemeIndex];
+		themeIconImg.src = nextTheme.themeIcon;
+		themeIconImg.alt = `${nextTheme.title} Icon`;
+		themeTitleDiv.innerHTML = nextTheme.title;
+	}
+}
 /* style themes ----------------------------END */
-let defaultTheme = themes.find( element => element.name === 'theme-dark');
+
+/* event handlers -------------------------BEGIN */
+themeSwitcherBtn.addEventListener('click', e => {
+	setNextTheme();
+});
+/* event handlers -------------------------END */
+
+let defaultTheme = themes.find( element => element.name === 'theme-light');
 initThemes(defaultTheme);
 
 console.log("hello!!!");
